3df8aaff62cad2c9a113f6b15dc23516e33b7b32
Palyon <meroveus5@gmail.com>
2024-12-07 14:26:22 -0600

    Updated to Minecraft 1.21.4

--- src/main/java/dev/custom/portals/util/ScreenTransitionPayload.java
+++ src/main/java/dev/custom/portals/util/ScreenTransitionPayload.java
@@ -7,7 +7,7 @@ import net.minecraft.network.packet.CustomPayload;
 
 public record ScreenTransitionPayload(Boolean isTransitioning) implements CustomPayload {
     public static final CustomPayload.Id<ScreenTransitionPayload> ID = new CustomPayload.Id<>(PortalHelper.SCREEN_TRANSITION_PACKET_ID);
-    public static final PacketCodec<RegistryByteBuf, ScreenTransitionPayload> CODEC = PacketCodec.tuple(PacketCodecs.BOOL, ScreenTransitionPayload::isTransitioning, ScreenTransitionPayload::new);
+    public static final PacketCodec<RegistryByteBuf, ScreenTransitionPayload> CODEC = PacketCodec.tuple(PacketCodecs.BOOLEAN, ScreenTransitionPayload::isTransitioning, ScreenTransitionPayload::new);
     @Override
     public Id<? extends CustomPayload> getId() {
         return ID;
